cb082e1270f607191567eb4c66c10efbbbfe78e4
Kamilla <kamilla.1201@gmail.com>
2019-01-26 16:14:34 -0800

    Updated initial config

--- initial_config.sh
+++ initial_config.sh
@@ -1,27 +1,21 @@
-# copy configs
-cp .tmux.conf ~
-cp .vimrc ~
-cp .zshrc ~
+apt-get update
+apt-get upgrade -y
 
-sudo apt install vim -y
-sudo apt-get install tmux -y
-sudo apt-get install pylint -y
-sudo apt-get install tsserver -y
-sudo apt install gnome-tweak-tool -y
-sudo apt install gufw -y
-sudo apt-get install vlc -y
-sudo apt-get install gimp -y
-sudo apt-get install shutter -y
-sudo apt-get install ubuntu-restricted-extras -y
+apt-get install vim -y
+apt-get install tmux -y
+apt-get install pylint -y
+apt-get install gnome-tweak-tool -y
+apt-get install gufw -y
+apt-get install vlc -y
+apt-get install gimp -y
+apt-get install shutter -y
+apt-get install ubuntu-restricted-extras -y
+apt-get install curl -y
 
-
-
-
-sudo ufw enable
+ufw enable
 
 # install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \\
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # install vim extensions
 git clone https://github.com/w0rp/ale.git ~/.vim/bundle/ale
@@ -31,14 +25,16 @@ git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # install nodejs
-sudo apt-get install nodejs
-sudo apt-get install npm
-sudo npm install --save typescript
-sudo npm install express-generator --save
-sudo npm install express --save
+apt-get install nodejs -y
+apt-get install npm -y
+npm install  -g eslint
 
-# zsh plugins
-sudo apt-get install zsh-syntax-highlighting
+# zsh
+apt-get install zsh -y
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
 
 # keyboard language switching
@@ -47,9 +43,21 @@ gsettings set org.gnome.desktop.wm.keybindings switch-input-source "['<Shift>Alt
 gsettings set org.gnome.desktop.wm.keybindings switch-input-source-backward "['<Alt>Shift_L']"
 
 # python
-python -m pip install --upgrade pip
-python -m pip install jupyter
-sudo pip install pandas
-sudo pip install numpy
-sudo pip install matplotlib
-sudo pip install python-language-server
+apt-get install python3.6
+apt-get install python3-distutils
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py && rm get-pip.py
+python3 -m pip install --upgrade pip
+python3 -m pip install setuptools
+python3 -m pip install jupyter
+python3 -m pip install pandas
+python3 -m pip install numpy
+python3 -m pip install matplotlib
+python3 -m pip install python-language-server
+python3 -m pip install opencv-python
+# copy configs
+cp .tmux.conf ~
+cp .vimrc ~
+cp .zshrc ~
+cp .pylintrc ~
